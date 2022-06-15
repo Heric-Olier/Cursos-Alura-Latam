fec55c0d877cced10022303d7b434066007b3a9b
Heric Olier <hericolier.code@gmail.com>
2022-06-15 09:24:45 -0500

    Initial commit

---
+++ Logica-de-Programacion-1-2-3/ensasyos.js
@@ -0,0 +1,40 @@
+
+/*let numbers = "110";
+
+let sum = 0;
+let binary = numbers.split("").reverse();
+
+for (let i = 0; i < binary.length; i++) {
+    sum += binary[i] * Math.pow(2, i);
+}
+console.log(sum);*/
+
+
+const cortar = (ingrediente) => {
+    console.log('Cortar ' + ingrediente);
+  }
+  
+  function mezclarIngredientes(n) {
+    if (n <= 0) return;
+  
+    console.log('Mezclar #' + n);
+    mezclarIngredientes(n - 1);
+  }
+  
+  function comer() {
+    console.log('Comer');
+  }
+  
+  function hacerEnsaladaMixta() {
+    cortar('tomate');
+    cortar('lechuga');
+    cortar('cebolla');
+    mezclarIngredientes(5);
+    comer();
+  }
+  
+  hacerEnsaladaMixta();
+
+
+
+
