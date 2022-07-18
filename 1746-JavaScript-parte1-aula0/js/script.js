e9888bb9365bd9a9e934ecb56f95fcb2940e7043
Heric Olier <hericolier.code@gmail.com>
2022-07-18 15:46:33 -0500

    script.js update

--- 1746-JavaScript-parte1-aula0/js/script.js
+++ 1746-JavaScript-parte1-aula0/js/script.js
@@ -1,3 +1,16 @@
-let titleContainer = document.querySelector('.container');
 
-titleContainer.textContent = 'Buena Vida Nutricion';
+
+let firstPatient = document.querySelector('#first__patient');
+let tdWeight = document.querySelector('.info-peso');
+let weight = tdWeight.textContent;
+let tdHeight = document.querySelector('.info-altura');
+let height = tdHeight.textContent;
+
+console.log(firstPatient);
+console.log(tdWeight);
+console.log(weight);
+console.log(tdHeight);
+console.log(height);
+
+let imc = weight / (height * height);
+console.log(imc);
