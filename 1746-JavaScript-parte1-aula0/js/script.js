2b2b708fdc442536b0828be5ab521b9df0bab474
Heric Olier <hericolier.code@gmail.com>
2022-07-19 22:38:13 -0500

    Update script.js

--- 1746-JavaScript-parte1-aula0/js/script.js
+++ 1746-JavaScript-parte1-aula0/js/script.js
@@ -3,14 +3,16 @@ let tdWeight = document.querySelector(".info-peso");    // peso
 let weight = tdWeight.textContent;      // peso do paciente
 let tdHeight = document.querySelector(".info-altura");   // altura
 let height = tdHeight.textContent;   // altura do paciente
+let tdImc = document.querySelector(".info-imc");  // imc
+
+
+let imc = weight / (height * height);   
+tdImc.textContent = imc.toFixed(2);  
+
 
 console.log(firstPatient);  
 console.log(tdWeight); 
 console.log(weight);           
 console.log(tdHeight);  
 console.log(height);       
-
-let imc = weight / (height * height);   
 console.log(imc);  
-document.write(imc);    
-
