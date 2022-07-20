fa8ce540bbcbef0bf16e84310244f2118aab8b5e
Heric Olier <hericolier.code@gmail.com>
2022-07-20 18:34:29 -0500

    Update script.js

--- 1746-JavaScript-parte1-aula0/js/script.js
+++ 1746-JavaScript-parte1-aula0/js/script.js
@@ -1,18 +1,29 @@
-let firstPatient = document.querySelector("#first__patient");   
-let tdWeight = document.querySelector(".info-peso");    // peso
-let weight = tdWeight.textContent;      // peso do paciente
-let tdHeight = document.querySelector(".info-altura");   // altura
-let height = tdHeight.textContent;   // altura do paciente
-let tdImc = document.querySelector(".info-imc");  // imc
+let patients = document.querySelectorAll(".paciente");
+
+for (let i = 0; i < patients.length; i++) {
+    let patient = patients[i];
+    let tdWeight = patient.querySelector(".info-peso"); // peso
+    let weight = tdWeight.textContent; // peso del paciente
+    let tdHeight = patient.querySelector(".info-altura"); // altura
+    let height = tdHeight.textContent; // altura del paciente
+    let tdImc = patient.querySelector(".info-imc"); // imc
+    
+    let weightIsValid = true;
+    let heightIsValid = true;
+    
+    if (weight < 0 || weight > 1000 || height < 0 || height > 3.0) {
+      tdImc.textContent = "Datos inválidos";
+      weightIsValid = false;
+      heightIsValid = false;
+    }
+    
+    if (weightIsValid && heightIsValid) {
+      let imc = weight / (height * height);
+      tdImc.textContent = imc.toFixed(2);
+    }
+    
+  }
+  
 
 
-let imc = weight / (height * height);   
-tdImc.textContent = imc.toFixed(2);  
 
-
-console.log(firstPatient);  
-console.log(tdWeight); 
-console.log(weight);           
-console.log(tdHeight);  
-console.log(height);       
-console.log(imc);  
