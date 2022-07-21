07aa8485d1e8aa6086e5868b1e7ba933ac076026
Heric Olier <hericolier.code@gmail.com>
2022-07-20 22:22:49 -0500

    Update script.js

--- 1746-JavaScript-parte1-aula0/js/script.js
+++ 1746-JavaScript-parte1-aula0/js/script.js
@@ -9,15 +9,12 @@ let addPatientBtn = document.querySelector("#adicionar-paciente"); // selecionam
 addPatientBtn.addEventListener("click", function (event) {
   event.preventDefault(); //con esta propiedad prevenimos que la pagina se recargue luego de darle click al boton
   let form = document.querySelector("#add-form"); // selecionamos el formulario
+  let table = document.querySelector("#tabla-pacientes"); // selecionamos la tabla
   let nameForm = form.nombre.value;
   let weightForm = form.peso.value;
   let heightForm = form.altura.value;
   let fatnessForm = form.gordura.value;
-  
-  console.log(nameForm);
-  console.log(weightForm);
-  console.log(heightForm);
-  console.log(fatnessForm);
+
 
   let patientTr = document.createElement("tr"); // creamos una nueva fila
   let nameTd = document.createElement("td"); // creamos una nueva columna
@@ -26,9 +23,26 @@ addPatientBtn.addEventListener("click", function (event) {
   let fatnessTd = document.createElement("td");  // creamos una nueva columna
   let imcTd = document.createElement("td");  // creamos una nueva columna
 
+  /* Agregamos los valores a las columnas */
+
+  nameTd.textContent = nameForm;
+  weightTd.textContent = weightForm;
+  heightTd.textContent = heightForm;
+  fatnessTd.textContent = fatnessForm;
+
+  patientTr.appendChild(nameTd);
+  patientTr.appendChild(weightTd);
+  patientTr.appendChild(heightTd);
+  patientTr.appendChild(fatnessTd);
+  patientTr.appendChild(imcTd);
+
+  table.appendChild(patientTr);
+  
 
 
-  //*! Validacion de los datos ingresados
+
+
+  /* Validacion de los datos ingresados */
 
   if (nameForm == "" || weightForm == "" || heightForm == "" || fatnessForm == "") {
     alert("Por favor, complete todos los campos");
