0ac54ceb920b779043ee3fa856f2cc432aca52d7
Heric Olier <hericolier.code@gmail.com>
2022-07-21 14:09:56 -0500

    Update form.js

--- 1746-JavaScript-parte1-aula0/js/form.js
+++ 1746-JavaScript-parte1-aula0/js/form.js
@@ -7,19 +7,34 @@ addPatientBtn.addEventListener("click", function (event) {
   let form = document.querySelector("#add-form"); // selecionamos el formulario
   let table = document.querySelector("#tabla-pacientes"); // selecionamos la tabla
   let patient = capturePatientData(form); // capturamos los datos del formulario
-  
+  let patientTr = buildPatientTr(patient); // creamos la fila del paciente
+  table.appendChild(patientTr);
 
+  /* Validacion de los datos ingresados */
+
+  if (
+    patient.nameForm == "" ||
+    patient.heightFormweightForm == "" ||
+    patient.heightForm == "" ||
+    patient.fatnessForm == ""
+  ) {
+    alert("Por favor, complete todos los campos");
+    return;
+  }
+});
+
+//! Creamos los elementos de la tabla de pacientes TR y TD
+
+let buildPatientTr = function (patient) {
   /* Crear los elementos de la tabla */
-
   let patientTr = document.createElement("tr"); // creamos una nueva fila
   let nameTd = document.createElement("td"); // creamos una nueva columna
-  let weightTd = document.createElement("td");  // creamos una nueva columna
-  let heightTd = document.createElement("td");  // creamos una nueva columna
-  let fatnessTd = document.createElement("td");  // creamos una nueva columna
-  let imcTd = document.createElement("td");  // creamos una nueva columna
+  let weightTd = document.createElement("td"); // creamos una nueva columna
+  let heightTd = document.createElement("td"); // creamos una nueva columna
+  let fatnessTd = document.createElement("td"); // creamos una nueva columna
+  let imcTd = document.createElement("td"); // creamos una nueva columna
 
   /* Agregamos los valores a las columnas */
-
   nameTd.textContent = patient.nameForm;
   weightTd.textContent = patient.weightForm;
   heightTd.textContent = patient.heightForm;
@@ -27,37 +42,26 @@ addPatientBtn.addEventListener("click", function (event) {
   imcTd.textContent = calculateImc(patient.weightForm, patient.heightForm);
 
   /* Agregamos las columnas a la fila */
-
   patientTr.appendChild(nameTd);
   patientTr.appendChild(weightTd);
   patientTr.appendChild(heightTd);
   patientTr.appendChild(fatnessTd);
   patientTr.appendChild(imcTd);
 
-  table.appendChild(patientTr);
+  return patientTr;
+};
 
-  /* Validacion de los datos ingresados */
 
-  if (patient.nameForm == "" || patient.heightFormweightForm == "" || patient.heightForm == "" || patient.fatnessForm == "") {
-    alert("Por favor, complete todos los campos");
-    return;
-  }
-});
+//! Capturamos los datos del formulario
 
 let capturePatientData = function (form) {
-    /* Obtenemos los datos del formulario */
+  /* Obtenemos los datos del formulario */
+  let patient = {
+    nameForm: form.nombre.value,
+    weightForm: form.peso.value,
+    heightForm: form.altura.value,
+    fatnessForm: form.gordura.value,
+  };
 
-    let patient = {
-      nameForm: form.nombre.value,
-      weightForm: form.peso.value,
-      heightForm: form.altura.value,
-      fatnessForm: form.gordura.value
-      
-    };
-
-    return patient; 
-}
-
-
-// agregamos a localStorage los nuevos pacientes
-
+  return patient;
+};
