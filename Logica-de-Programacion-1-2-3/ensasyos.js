b663c288263e561ef346444f6b53d825bcca97a3
Heric Olier <hericolier.code@gmail.com>
2022-07-22 23:50:48 -0500

    Update ensasyos.js

--- Logica-de-Programacion-1-2-3/ensasyos.js
+++ Logica-de-Programacion-1-2-3/ensasyos.js
@@ -1,4 +1,3 @@
-
 /*let numbers = "110";
 
 let sum = 0;
@@ -9,32 +8,27 @@ for (let i = 0; i < binary.length; i++) {
 }
 console.log(sum);*/
 
-
 const cortar = (ingrediente) => {
-    console.log('Cortar ' + ingrediente);
-  }
-  
-  function mezclarIngredientes(n) {
-    if (n <= 0) return;
-  
-    console.log('Mezclar #' + n);
-    mezclarIngredientes(n - 1);
-  }
-  
-  function comer() {
-    console.log('Comer');
-  }
-  
-  function hacerEnsaladaMixta() {
-    cortar('tomate');
-    cortar('lechuga');
-    cortar('cebolla');
-    mezclarIngredientes(5);
-    comer();
-  }
-  
-  hacerEnsaladaMixta();
+  console.log("Cortar " + ingrediente);
+};
 
+function mezclarIngredientes(n) {
+  if (n <= 0) return;
 
+  console.log("Mezclar #" + n);
+  mezclarIngredientes(n - 1);
+}
 
+function comer() {
+  console.log("Comer");
+}
 
+function hacerEnsaladaMixta() {
+  cortar("tomate");
+  cortar("lechuga");
+  cortar("cebolla");
+  mezclarIngredientes(5);
+  comer();
+}
+
+hacerEnsaladaMixta();
