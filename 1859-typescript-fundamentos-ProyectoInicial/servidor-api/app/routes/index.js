3bd22a46b1e05c12379877bc6ea4252f5e50a1e8
Heric Olier <hericolier.code@gmail.com>
2023-03-20 20:46:20 -0500

    Update index.js

--- 1859-typescript-fundamentos-ProyectoInicial/servidor-api/app/routes/index.js
+++ 1859-typescript-fundamentos-ProyectoInicial/servidor-api/app/routes/index.js
@@ -1,9 +1,8 @@
 /* Código simplório, apenas para fornecer o serviço para a aplicação */
 
-var api = require('../api');
+var api = require("../api");
 
-module.exports  = function(app) {
-        
-    app.route('/datos')
-        .get(api.datos);          
+module.exports = function (app) {
+  app.route("/api").get(api.index);
+  app.route("/datos").get(api.datos);
 };
