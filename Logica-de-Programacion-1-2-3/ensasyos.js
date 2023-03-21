04dad3f74213f25ff4e717c24fb10395eda4e456
Heric Olier <hericolier.code@gmail.com>
2023-03-20 20:47:47 -0500

    Update ensasyos.js

--- Logica-de-Programacion-1-2-3/ensasyos.js
+++ Logica-de-Programacion-1-2-3/ensasyos.js
@@ -1,12 +1,3 @@
-/*let numbers = "110";
-
-let sum = 0;
-let binary = numbers.split("").reverse();
-
-for (let i = 0; i < binary.length; i++) {
-    sum += binary[i] * Math.pow(2, i);
-}
-console.log(sum);*/
 
 const cortar = (ingrediente) => {
   console.log("Cortar " + ingrediente);
